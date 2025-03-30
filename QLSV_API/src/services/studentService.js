4ab60478c422eac3b53036e7d4b8886a9b16931a
Dthuong812 <120299153+Dthuong812@users.noreply.github.com>
2025-03-30 15:35:49 +0700

    Update studentService.js

--- QLSV_API/src/services/studentService.js
+++ QLSV_API/src/services/studentService.js
@@ -52,7 +52,7 @@ module.exports = {
             email: student.email
         }, process.env.JWT_SECRET, {expiresIn: "7d"});
         console.log("Generated Token:", token);
-        return {message: "Đăng nhập thành công!", token, name: student.name,email:student.email,studentId:student.student_id,studentClas:student.class };
+        return {message: "Đăng nhập thành công!", token,id: student._id, name: student.name,email:student.email,studentId:student.student_id,studentClas:student.class };
     },
     // Lấy danh sách sinh viên (hỗ trợ phân trang)
     getStudentsService: async (page = 1, limit = 10) => {
